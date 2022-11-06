8f218d2e8f1a6c08912f42c319c5cfed23e9cbee
Sayantan majee <sayantan.majee24@gmail.com>
2022-11-06 10:59:30 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Sayantan Majee</title>
+      </Head>
+
+      <h1 className="text-3xl font-bold underline">Hello this is sayantan</h1>
+    </div>
+  )
+}
